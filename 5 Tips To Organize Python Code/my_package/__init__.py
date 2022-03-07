7fa6a2dcba3b3f4d24652526ddcc74a4c8212082
quenrythane <artur.babinski.g@gmail.com>
2022-03-07 11:39:53 +0100

    5 Tips To Organize Python Code

---
+++ 5 Tips To Organize Python Code/my_package/__init__.py
@@ -0,0 +1,4 @@
+from .package_file_1 import function_file_1
+# . mean current directory
+
+print('my_package imported')  # initial message (doesn't required)
---
+++ 5 Tips To Organize Python Code/my_package/package_file_1.py
@@ -0,0 +1,2 @@
+def function_file_1():
+    print('message package_file_1')
